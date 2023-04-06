f9b26762f5abdc90ca4c9983b3e50b98fcc00cd3
loganwemploy <57770383+loganwemploy@users.noreply.github.com>
2023-04-06 02:53:04 -0500

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,53 @@
+{
+"videos": [
+{
+"title": "A tour of Glidewell Laboratory",
+"url": "//www.youtube.com/embed/mjB6Zm39Lno"
+},
+{
+"title": "002",
+"url": "https://www.youtube.com/embed/s_KS_NjZ6DA"
+},
+{
+"title": "Shatter Your Doubts with the Sledgehammer Test",
+"url": "//www.youtube.com/embed/pYkwcehadxo"
+},
+{
+"title": "BruxZir Solid Zirconia vs. PFM: Gravity Bites",
+"url": "//www.youtube.com/embed/TOmOXKMnirw"
+},
+{
+"title": "Using BruxZir<sup>&reg;</sup> Crowns To Mask Tetracycline & Correct Edge-To-Edge Bite",
+"url": "//www.youtube.com/embed/qFXMJTxrWds"
+},
+{
+"title": "BruxZir<sup>&reg;</sup>: How It Began",
+"url": "//www.youtube.com/embed/2FJqZOI0ktM"
+},
+{
+"title": "BruxZir<sup>&reg;</sup> Solid Zirconia Patient Education",
+"url": "//www.youtube.com/embed/D4taHSdbP_w"
+},
+{
+"title": "Digital Impressions Utilizing Intra Oral Scanning Technology",
+"url": "//www.youtube.com/embed/eWeXwb9TGFg"
+},
+{
+"title": "aveoTSD Patient Testimonial",
+"url": "//www.youtube.com/embed/SBlb5SH2n1s"
+},
+{
+"title": "2010 Tosoh Award Winner",
+"url": "//www.youtube.com/embed/hUmcLizNJqM"
+},
+{
+"title": "IOS FastScan Digital Impression & Modeling System",
+"url": "//www.youtube.com/embed/09qKu_TI7b0"
+},
+{
+"title": "Introduction to BruxZir Total Zirconia",
+"url": "//www.youtube.com/embed/tUpp_DOY4Vw"
+}
+]
+}
+
